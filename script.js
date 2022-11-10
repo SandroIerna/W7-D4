13982b97e52e71be3166fc3ba2ed4f9de2d74ba8
SandroIerna <iernasandro@gmail.com>
2022-11-10 15:49:21 +0100

    adds Backoffice

--- script.js
+++ script.js
@@ -1,6 +1,54 @@
-fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
-  headers: {
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
-  },
-});
+const headers = {
+  "Content-Type": "application/json",
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
+};
+
+// const options = {
+//   method: "POST",
+//   body: JSON.stringify(newProduct),
+//   headers: {
+//     "Content-Type": "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
+//   },
+// };
+
+post = async () => {
+  let data = await fetch(
+    "https://striveschool-api.herokuapp.com/api/product/",
+    options
+  );
+  console.log(data);
+};
+
+// Function that is called on the "Submit Button"
+const handleSubmit = async (event) => {
+  event.preventDefault();
+
+  const newProduct = {
+    name: document.getElementById("name").value,
+    description: document.getElementById("description").value,
+    brand: document.getElementById("product-brand").value,
+    imageUrl: document.getElementById("image-src").value,
+    price: document.getElementById("price").value,
+  };
+
+  let response = await fetch(
+    "https://striveschool-api.herokuapp.com/api/product/",
+    {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: headers,
+    }
+  );
+  try {
+    if (!response.ok) throw new Error("Generic error!");
+    if (response.ok) {
+      const event = await response.json();
+      alert("Event created successfully!");
+    }
+  } catch (error) {
+    alert("Something went wrong, " + error);
+  }
+};
