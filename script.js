30b573e381e5dbcc93568fe4eaa3da7649ddce62
SandroIerna <iernasandro@gmail.com>
2022-11-10 16:57:07 +0100

    adds more solutions

--- script.js
+++ script.js
@@ -4,20 +4,14 @@ const headers = {
     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
 };
 
-// const options = {
-//   method: "POST",
-//   body: JSON.stringify(newProduct),
-//   headers: {
-//     "Content-Type": "application/json",
-//     Authorization:
-//       "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
-//   },
-// };
-
-post = async () => {
+loadProducts = async () => {
   let data = await fetch(
     "https://striveschool-api.herokuapp.com/api/product/",
-    options
+    {
+      method: "POST",
+      body: JSON.stringify(newProduct),
+      headers: headers,
+    }
   );
   console.log(data);
 };
