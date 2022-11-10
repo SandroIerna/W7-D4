eaa4bad5d943369c4d15df3691ff30207e1dd017
SandroIerna <iernasandro@gmail.com>
2022-11-10 14:16:41 +0100

    adds boilerplate

---
+++ script.js
@@ -0,0 +1,6 @@
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzZjZjg2NGQ0YmUzZDAwMTU4NDYwMWEiLCJpYXQiOjE2NjgwODU4NjEsImV4cCI6MTY2OTI5NTQ2MX0.dScw6478XOLngE_XoEBv65SaC9agW9OGQhwrpVvQsII",
+  },
+});
